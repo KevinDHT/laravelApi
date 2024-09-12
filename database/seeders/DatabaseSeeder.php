5bb496a7414ef3fc0566d360bd4739128058bbe8
Kevin Daniel Hernandez Tello <s21030147@itsch.edu.mx>
2024-09-12 09:23:59 -0600

    Se crearon y configuraron las migraciones, factories, seed y la relacion muchos a muchos del modelo receta

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,12 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Receta;
+use App\Models\Categoria;
+use App\Models\Etiqueta;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Database\Factories\EtiquetaFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +18,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(29)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Kevin Daniel Hernandez Tello',
+            'email' => 'kevindht@admin.com',
         ]);
+
+        Categoria::factory(10)->create();
+        Receta::factory(100)->create();
+        Etiqueta::factory(50)->create();
+
+        //Relacion muchos a muchos
+        $recetas = Receta::all();
+        $etiquetas = Etiqueta::all();
+
+        foreach($recetas as $receta){
+            $receta->etiquetas()->attach($etiquetas->random(rand(2,4)));
+
+        }
+
     }
 }
